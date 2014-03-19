518c6725f490a28333c4cd47f18d3da2ff92b747
Zak Stern <zak.stern@gmail.com>
2014-03-18 22:45:46 -0700

    create point.java and start brute.java

---
+++ Collinear/Brute.java
@@ -0,0 +1,16 @@
+/*************************************************************************
+ * Name: Zak Stern
+ * Email: 
+ *
+ * Compilation:  javac Brute.java
+ * Execution:
+ * Dependencies: Point.java
+ *
+ * Description: examines 4 points at a time and checks whether they all lie on the same line segment, 
+ * printing out any such line segments to standard output and drawing them using standard drawing.
+ *
+ *************************************************************************/
+
+public class Brute {
+    
+}
